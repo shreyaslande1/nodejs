ac1fed64f04b56b1324489f874707a2ae18e3d25
shreyaslande <shreyaslande200@gmail.com>
2026-07-06 14:10:16 +0530

    url lecture

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 const myserver = http.createServer((req, res) => {
+    if(req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()}: ${req.url} New Req Received\n`
+    const myurl = url.parse(req.url, true);
+    console.log(myurl)
   fs.appendFile('log.txt', log, (err,data)=>{
-    switch(req.url){
+
+    switch(myurl.pathname){
         case "/":
             res.end("homepage");
             break;
         case "/about":
-            res.end("aboutpage");
+            const username = myurl.query.myname
+
+            res.end(`hi, ${username}`);
+
+            break;
+        case "/search":
+            const search = myurl.query.search_query
+            res.end(`here is the result for search `+search)
             break;
         case "/contact":
             res.end("contact page");
