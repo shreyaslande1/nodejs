2327998d251ba2b35496cf72bb29bf6e2318d788
shreyaslande <shreyaslande200@gmail.com>
2026-07-07 16:04:34 +0530

    rest apis making

---
+++ project01/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json")
+const app = express();
+
+const PORT = 8000;
+// app.get("/users", (req, res)=>{
+//     const html = `
+//         <ul>
+//         ${users.map((user)=>`<li>${user.first_name}</li>`)}
+//         </ul>
+//     `;
+//     res.send(html)
+// })
+app.route("/api/users/:id")
+.get((req, res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=>user.id===id);
+    return res.json(user)
+}).patch((req, res)=>{
+    return res.json({status:"pending"})
+}).delete((req, res)=>{
+    return res.json({status:"pending"})
+})
+
+app.post("/api/users", (req, res)=>{
+    return res.json({status:"pending"})
+});
+
+app.get("/api/users", (req, res)=>{
+    return res.json(users)
+})
+
+app.listen(PORT, ()=>{console.log("server is started on port 8000")})
