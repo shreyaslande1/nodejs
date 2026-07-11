ed985d48d331fe62b04cdba85163789440214099
shreyaslande <shreyaslande200@gmail.com>
2026-07-11 15:13:47 +0530

    completed fileupload

--- imageupload/index.js
+++ imageupload/index.js
@@ -8,12 +8,14 @@ const PORT = 8000;
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        return cb(null, "/uploads");
+        return cb(null, "./uploads");
     },
     filename: function(req, file, cb){
         return cb(null, `${Date.now()}-${file.originalname}`)
     },
 })
+
+const upload = multer({storage});
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
